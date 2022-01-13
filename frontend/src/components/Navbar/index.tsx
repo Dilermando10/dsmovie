120405777e05a1861ee0ea88cfc56a20411b30a2
Dilermando <dilermando10@gmail.com>
2022-01-13 17:54:15 -0300

    _redirects

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,29 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
-import './styles.css'
+import './styles.css';
 function Navbar() {
+
     return (
         <header>
             <nav className="container">
-                <div className="dsmovie-nav-content">
-                    <h1>DSMovie</h1>
-                    <a href="https://github.com/Dilermando10">
+                <div>
+                    <div className="dsmovie-nav-content">
+                        <h1>DSMovie</h1>
+                        <a href="https://github.com/Dilermando10"></a>
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-<<<<<<< HEAD
-                            <p className="dsmovie-contact-link">/Dilermando</p>
-=======
-                            <p className="dsmovie-contact-link">/liperossell</p>
->>>>>>> a9f11ec582f749fb9384e61a8b38f86ba78c279b
+                            <p className="dsmovie-contact-link"> target="_blank" rel="Dilermando"</p>
                         </div>
-                    </a>
+                    </div>
                 </div>
             </nav>
-        </header>
-    );
-}
+        </header>);
 
-<<<<<<< HEAD
+}
 export default Navbar;
-=======
-export default Navbar;
->>>>>>> a9f11ec582f749fb9384e61a8b38f86ba78c279b
